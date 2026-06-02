bf4ddcf2fbb63c7bbef04f70f8e3b261921da8cc
tulk2209 <tulk.2209@gmail.com>
2026-06-03 04:12:44 +0700

    123

--- scripts.js
+++ scripts.js
@@ -44,7 +44,7 @@ function updateCeritifcate() {
     if(imgBackground.complete)
         ctx.drawImage(  imgBackground, 0, 0, canvas.width, canvas.height); // Or at whatever offset you like
     
-    var xPos = 1000, yPos = 2500;
+    var xPos = 1500, yPos = 2500;
     ctx.textAlign = "center";
     ctx.fillStyle = "#eff2ff";
     ctx.shadowColor = "#000";
